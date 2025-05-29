59fc519451be2a8ff4fe5b0c81bff523fffa3cb6
ccatura <charlie@charliecatura.com>
2025-05-29 16:04:39 -0400

    Removed pword

--- connect.py
+++ connect.py
@@ -1,9 +1,16 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load variables from .env file
+
 
 def get_connection():
     return mysql.connector.connect(
         host="sql647.main-hosting.eu",
         user="u682819236_scroll2",
-        password="8h:pPw>m8Nx;",
+        password=os.getenv("DB_PASSWORD"),
         database="u682819236_music_scroll2"
     )
+
+
